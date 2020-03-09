22939216f03da014e7540b7a88433dc454a969cc
Emre <ava@AVAs-MBP.fios-router.home>
2020-03-09 16:41:40 -0400

    - Can use chan id X

--- src/server/index.js
+++ src/server/index.js
@@ -9,24 +9,6 @@ const history = require('connect-history-api-fallback');
 const {beforeMiddleware} = require('./configure');
 
 
-
-
-
-// const axios = require('axios').default;
-
-// const APP_ENV = process.env.VUE_APP_ENV || "production";
-// const isDev = (APP_ENV==="development");
-
-// const AVA_IP = process.env.AVA_IP || "localhost";
-// const AVA_PORT = process.env.AVA_PORT || 9650;
-// const AVA_PROTOCOL = process.env.AVA_PROTOCOL || "https";
-// const AVA_NETWORK_ID = process.env.AVA_NETWORK_ID || "12345";
-
-// const PK =  process.env.PRIVATE_KEY; // The private key that holds the given assets to supply the faucet
-// let ASSET_ID = process.env.ASSET_ID; // Which asset is being sent from the faucet
-// const DROP_SIZE =  process.env.DROP_SIZE || 100; // how much of the given asset to transfer from the faucet
-
-
 // VALUE CHECKING ####################################################
 if(!CONFIG.PK){
     console.error("You must start the server with a valid PRIVATE_KEY.");
@@ -46,7 +28,7 @@ const app = express();
 beforeMiddleware(app);
 
 
-// UI
+// Https rerouting
 app.use((req, res, next) => {
     if (req.secure) {
         // request was via https, so do no special handling
@@ -60,25 +42,10 @@ app.use((req, res, next) => {
 // Serve static files
 const publicPath = resolve(__dirname, '../../dist');
 const staticConf = { maxAge: '1y', etag: false };
+
 app.use(express.static(publicPath, staticConf));
-
-
 app.use(history());
 
-
-
-
-
-
-// async function start(){
-//     if(!ASSET_ID){
-//         let res = await avm.getAssetDescription('AVA');
-//         ASSET_ID = bintools.avaSerialize(res.assetID);
-//     }
-//
-//
-// }
-// start();
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
@@ -95,17 +62,3 @@ app.listen(port, () => {
     }
 
 });
-
-// const port = process.env.PORT || 4000;
-// app.listen(port, () => {
-//     console.log(`listening on \t${port}`);
-//     console.log(`Environment: \t${APP_ENV}`);
-//     console.log("Droplet size: \t",DROP_SIZE);
-//     console.log("Asset Id: \t",ASSET_ID);
-//     console.log("Faucet Address: \t",FAUCET_ADDRESS);
-//
-//     avm.getUTXOs([FAUCET_ADDRESS]).then(utxos => {
-//         let balance = utxos.getBalance([FAUCET_ADDRESS], ASSET_ID);
-//         console.log("Available Balance: ",balance.toString());
-//     });
-// });
