43a9dd8043c1c94f580da79f8e0165d27debe228
Emre Kanatli <kanatliemre@gmail.com>
2020-09-04 00:48:22 -0400

    trying latest block

--- src/server/eth.js
+++ src/server/eth.js
@@ -1,6 +1,7 @@
 const Web3 = require('web3');
 
 // Get constants
+const axios = require('axios');
 
 const PK = process.env.PRIVATE_KEY_C; // The private key that holds the given assets to supply the faucet
 const txAmount = process.env.DROP_SIZE_C || "200000000000000000";
@@ -8,7 +9,8 @@ const AVA_IP = process.env.AVA_IP || "localhost";
 const AVA_PORT = process.env.AVA_PORT || "9650";
 const AVA_PROTOCOL = process.env.AVA_PROTOCOL || "http";
 
-const GAS_PRICE = "47000000000";
+// const GAS_PRICE = "47000000000";
+const GAS_PRICE = "97000000000";
 
 const CONFIG_C = {
   PK: PK,
@@ -30,15 +32,32 @@ web3.eth.getBalance(account.address).then(res => {
     console.log(`(C) Address: `,account.address)
 });
 
+
+async function getAcceptedTxCount(){
+    let json = {
+        jsonrpc: "2.0",
+        method: "eth_getTransactionCount",
+        params: [account.address, "accepted"],
+        id: 1
+    }
+    let res = await axios.post(rpcUrl, json);
+    let hex = res.data.result;
+    let num = parseInt(hex, 16);
+    return num
+}
+
 // !!! Receiver is given in 0x format
 async function sendAvaC(receiver){
 
-    let pendingTx = await web3.eth.getTransactionCount(account.address, 'pending');
+    // let pendingTx = await web3.eth.getTransactionCount(account.address, 'pending');
     let latestTx = await web3.eth.getTransactionCount(account.address, 'latest');
-    let txDiff = pendingTx - latestTx;
-    let nonce = await web3.eth.getTransactionCount(account.address);
-        nonce += txDiff;
+    // let acceptedTx = await getAcceptedTxCount();
 
+    // let txDiff = pendingTx - acceptedTx;
+    // let nonce = await web3.eth.getTransactionCount(account.address);
+    //     nonce += txDiff;
+
+    console.log(latestTx);
 
     const txConfig = {
         from: account.address,
@@ -47,8 +66,7 @@ async function sendAvaC(receiver){
         to: receiver,
         value: txAmount,
         data: "",
-        // nonce: nonce,
-        // chain: "43110"
+        nonce: latestTx+1,
     };
 
     let signedTx = await account.signTransaction(txConfig);
